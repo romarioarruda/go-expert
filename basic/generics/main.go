bd8acd0a3fe2d49c1d1a765347ec48427e59b247
Romário Dev <romarioarruda98@gmail.com>
2023-08-26 10:17:24 -0300

    add basic concepts about comparable generics

--- basic/generics/main.go
+++ basic/generics/main.go
@@ -10,10 +10,18 @@ func sum[T Number](a T, b T) T {
 	return a + b
 }
 
+//comparable values must be the same type
+func compare[T comparable](a, b T) bool {
+	return a == b
+}
+
 func main() {
 	resultInt := sum(10, 10)
 	fmt.Printf("resultInt: %v, type: %T\n", resultInt, resultInt)
 
 	resultFloat := sum(10.444, 8.7)
 	fmt.Printf("resultFloat: %v, type: %T\n", resultFloat, resultFloat)
+
+	compareResult := compare(10, 10)
+	fmt.Printf("Compare result: %v\n", compareResult)
 }
