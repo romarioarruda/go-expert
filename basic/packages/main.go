d3420e293438656741b980099338b300f5c8090b
Romário Dev <romarioarruda98@gmail.com>
2023-08-26 10:37:02 -0300

    adjust import of the math package

--- basic/packages/main.go
+++ basic/packages/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/romarioarruda/go-expert/math"
+	"github.com/romarioarruda/go-expert/basic/packages/math"
 )
 
 func main() {
-	result := math.Sum(10, 10)
-	fmt.Println("The sum result is: %v", result)
+	result := math.Sum(10, 30)
+	fmt.Println("The sum result is:", result)
 }
