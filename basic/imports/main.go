dbd53ec8b33c8d43328c3cf43f6450735a1e2b10
Romário Dev <romarioarruda98@gmail.com>
2023-08-26 09:51:24 -0300

    add basic concepts about module imports

---
+++ basic/imports/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	teste := "Teste 123"
+	fmt.Printf("O tipo da variavel é: %T\n", teste)
+	fmt.Printf("O resultado da variavel é: %v\n", teste)
+}
