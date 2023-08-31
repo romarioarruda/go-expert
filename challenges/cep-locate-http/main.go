c1384be8019ee0c0bb0728918ad0c0d4b8b6fbb3
Romário Dev <romarioarruda98@gmail.com>
2023-08-31 10:03:33 -0300

    cep-locate-http): add func to get cep result

--- challenges/cep-locate-http/main.go
+++ challenges/cep-locate-http/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
+	"go-expert/challenges/cep-locate/structs"
 )
 
 func main() {
-	http.HandleFunc("/cep", FindCep)
+	http.HandleFunc("/cep", HandleCep)
 
 	http.ListenAndServe(":8080", nil)
 }
 
-func FindCep(resp http.ResponseWriter, req *http.Request) {
+func HandleCep(resp http.ResponseWriter, req *http.Request) {
 	resp.Header().Set("Content-Type", "application/json")
 
 	cepParam := req.URL.Query().Get("number")
@@ -20,5 +23,42 @@ func FindCep(resp http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	resp.Write([]byte(`{"msg": "Hello World"}`))
+	result, err := FindCep(cepParam)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"msg": "There was an error to get cep"}`))
+		return
+	}
+	
+	finalJson, err := json.Marshal(result)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"msg": "There was an error trying to convert json result"}`))
+		return
+	}
+
+	resp.Write(finalJson)
+}
+
+func FindCep(cep string) (*viacep.ViaCep, error) {
+	req, err := http.Get("https://viacep.com.br/ws/"+ cep +"/json/")
+	if err != nil {
+		return nil, err
+	}
+
+	defer req.Body.Close()
+
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var cepResult viacep.ViaCep
+
+	err = json.Unmarshal(body, &cepResult)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cepResult, nil
 }
