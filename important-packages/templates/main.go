41871b4156602fcc341afd2d816236f23050ddf2
Romário Dev <romarioarruda98@gmail.com>
2023-09-02 11:54:45 -0300

    study(templates): working wiht html templates

---
+++ important-packages/templates/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"html/template"
+)
+
+type Course struct {
+	Name string
+	Hour int
+}
+
+type Courses []Course
+
+func main() {
+	htmlTemplates := []string{
+		"important-packages/templates/header.html",
+		"important-packages/templates/content.html",
+		"important-packages/templates/footer.html",
+	}
+
+	tmp := template.Must(
+		// template.New("MyTemplate").Parse("Course: {{ .Name }} - Hour: {{ .Hour }}"),
+		template.New("content.html").ParseFiles(htmlTemplates...),
+	)
+
+	//If you working with http, replace os.Stdout for http.ResponseWriter
+	err := tmp.Execute(os.Stdout, Courses{
+		{ "Go", 40 },
+		{ "Python", 30 },
+		{ "PHP", 20 },
+	})
+
+	if err != nil {
+		panic(err)
+	}
+}
