09ff419dc52b2cf3a86852722877e954c36393d7
Romário Dev <romarioarruda98@gmail.com>
2023-09-27 09:02:09 -0300

    test(unit-test): add unit test with testing module

---
+++ testing/example-1/tax_test.go
@@ -0,0 +1,32 @@
+package tax
+
+import "testing"
+
+// func TestCalculateTax(t *testing.T) {
+// 	amount := 1000.0
+// 	expected := 5.0
+
+// 	result := CalculateTax(amount)
+
+// 	if result != expected {
+// 		t.Errorf("Expected %f but got %f", expected, result)
+// 	}
+// }
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct { amount, expected float64 }
+	table := []calcTax{
+		{ 0.0, 0.0 },
+		{ 100.0, 5.0 },
+		{ 999.9, 5.0 },
+		{ 1000.1, 10.0 },
+	}
+
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+
+		if result != item.expected {
+			t.Errorf("Expected %f but got %f", item.expected, result)
+		}
+	}
+}
