9ab86721b2b749f8f839ab0ea604966c82b17e64
Romário Dev <romarioarruda98@gmail.com>
2023-12-16 14:48:19 -0300

    feat(basics): revisit basic concepts

--- basic/packages/math/math.go
+++
@@ -1,6 +0,0 @@
-package math
-
-//Starting with capital letter means that Sum is automatically exported (same thing as module.export or export default in Node.js)
-func Sum[T int | float64](a, b T) T {
-	return a + b
-}
